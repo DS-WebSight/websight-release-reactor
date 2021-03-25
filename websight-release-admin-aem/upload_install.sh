2c7980bbdd26477b1feb0e8421a42a3fa2f36254
websight-sync <admin@ds.pl>
2021-03-25 11:13:02 +0100

    Release of  websight-release-reactor - 1.0.3

---
+++ websight-release-admin-aem/upload_install.sh
@@ -0,0 +1,1 @@
+curl -u admin:admin -F file=@"target/websight-release-admin-aem-1.0.2.zip" -F name="pl.ds.websight:websight-release:1.0.2" -F force=true -F install=true http://localhost:4502/crx/packmgr/service.jsp
---
+++ websight-release-admin-sling/upload_install.sh
@@ -0,0 +1,1 @@
+curl -u admin:admin -F file=@"target/websight-release-admin-sling-1.0.0.zip" -F name="pl.ds.websight:websight-release-admin-sling:1.0.0" -F force=true -F install=true http://localhost:4502/crx/packmgr/service.jsp
